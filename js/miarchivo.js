5a22ea922479f352b268d39de4d73baf9bfd1406
juan benitez <juanignacioybr@gmail.com>
2022-09-03 07:52:30 -0300

    desafio

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,7 +1,7 @@
-const IVA = 1.21
 const libros = []
 const recomendaciones = []
 const Ventas = ['los 11 pasos', 'padre rico', 'y soles lloveran', 'el otro camino', 'la solucion es js']
+const IVA = 1.21
 
 class Recomendacion {
 constructor(nombreLibro, añoLibro) {
@@ -16,12 +16,14 @@ class Plantilla {
     this.nombre = nombre
     this.importe = importe 
     }
+
 precioFinal() {
     return parseFloat((this.importe * IVA).toFixed(2))
     }
 }
 
-function creoID() { return parseInt(Math.random() * 10000) }
+function creoID() { return parseInt(Math.random() * 10000)
+ }
 function generadorAutomatico() {
     libros.push(new Plantilla(1234, "los 11 pasos de la magia", 3000))
     libros.push(new Plantilla(2345, "padre rico", 2000))
@@ -39,10 +41,9 @@ function almacenamiento () {
 }
 almacenamiento()
 
-
 function ordenarLibros() {
-    console.clear()
-    console.table(libros)
+console.clear()
+console.table(libros)
 libros.sort((a, b) => {
 if (a.importe > b.importe) {
     return 1
@@ -69,57 +70,132 @@ libros.forEach(producto => {
     })
 }
 
+const good = ()=> {
+    Swal.fire({
+        title: 'Error!', //success,
+        text: 'Do you want to continue',
+        icon: 'success',
+        showConfirmButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Acpetar",
+        confirmButtonText: "Cancelar",
+        //confirmButtonText: 'Cool'
+      })
+} 
+const no = ()=> {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    }) }
+
 function existeLibro() {
-    let codigo = prompt("Ingresa el código de producto:")
-    const resultado = libros.some(Plantilla => Plantilla.id === parseInt(codigo))
-    console.log("Existe?", resultado)
+let codigo = prompt("Ingresa el código de producto:")
+const resultado = libros.some(Plantilla => Plantilla.id === parseInt(codigo))
+console.log("Existe?", resultado)
 if (resultado == true) {
-    alert("si lo tenemos en stock!!!")
+    good()
     }
 else {
-    //alert("no tenemos ese libroo")
-    toastSwal("Completa todos los valores solicitados.", 'warning', "#DB4914")
+    no()
     }
 }
-const toastSwal = (mensaje, icono, bgcolor)=> {
-    Swal.fire ({
-        //title: 'Error!', //success,
-        //title: "titulo",
-        toast: true,
-        position: "top-end", // bottom-end  top start bottom-start  x defecto: center
-        text: mensaje ,
-        icon: icono,
-        showCancelButton: false,
-        timer: 3000,
-        background: bgcolor,
-        color:"white"
-      })
-}
+
 
 function cargoArrayVenta() {
-    Ventas.sort().reverse()
+Ventas.sort().reverse()
 Ventas.forEach(pais => {
-    selectLibro.innerHTML += `<option value="">${pais}</option>`
+selectLibro.innerHTML += `<option value="">${pais}</option>`
     })
 }
 
 function agregarRecomendacion() {
-    let nombreLibro = prompt("Ingresa el nombre del libro:")
-    let añoLibro = parseInt(prompt("Ingresa el año del libro:"))
-    recomendaciones.push(new Recomendacion(nombreLibro, añoLibro))
-    recomendaciones.push(new Recomendacion("BIBLIA", 01))
-    recomendaciones.push(new Recomendacion("TORA", -504))
-    recomendaciones.push(new Recomendacion("Mahabharata ", -300))
-    console.table(recomendaciones)
+guardarProducto()
+ nuevoProducto()
+recomendaciones.push(new Recomendacion("BIBLIA", 01))
+recomendaciones.push(new Recomendacion("TORA", -504))
+recomendaciones.push(new Recomendacion("Mahabharata ", -300))
+console.table(recomendaciones)
 }
 
 function recoLibro() {
-    const reco = document.getElementById("reco")
-    reco.innerHTML = ""
+const reco = document.getElementById("reco")
+reco.innerHTML = ""
 recomendaciones.forEach(producto => {
-    reco.innerHTML += `<tr>
-                           <td>${producto.nombreLibro}</td>
-                           <td>${producto.añoLibro}</td>
+reco.innerHTML += `<tr>
+                           <td>${producto.descripcion}</td>
+                           <td>${producto.importa}</td>
                        </tr>`
     })
 }
+
+const rees = [] 
+const carrito = [] 
+
+class Ree {
+    constructor(id, descrip, impor) {
+        this.id = id
+        this.descrip = descrip
+        this.impor = impor
+    }}   
+
+function creoID() { return parseInt(Math.random() * 10000) }
+
+const codige = document.querySelector("#codige")
+const descripcion = document.querySelector("#descripcion")
+const importa = document.querySelector("#importe")
+
+const btnNuevo = document.querySelector("#btnNuevo")
+const btnGuardar = document.querySelector("#btnGuardar")
+
+btnNuevo.addEventListener("click", nuevoProducto)
+btnGuardar.addEventListener("click", guardarProducto)
+
+function guardarDatos() {
+localStorage.setItem("#descripcion", descripcion.value)
+localStorage.setItem("#importe", importa.value)
+}
+
+function guardarProducto() {
+    if (codige.value != "" && descripcion.value !="" && importa.value > 0 ) {
+ const prod = { 
+ id: codige.value,
+ descripcion: descripcion.value, 
+  importa: importa.value 
+ }
+rees.push(prod) 
+localStorage.setItem("ree", JSON.stringify(rees))
+    cargarProductas() 
+    }
+}
+
+function nuevoProducto() {
+codige.value = creoID()
+descripcion.value =""
+importa.value =""
+descripcion.focus () 
+}
+
+function recuperoDeLS() {
+    if(localStorage.rees) {
+        const prodGuardados = JSON.parse(localStorage.getItem("productos"))
+        prodGuardados.forEach(prod => {
+            rees.push(prod)
+        });
+        console.table(prodGuardados)
+    } 
+} 
+
+ function cargarProductas() {
+const cuerpo = document.querySelector("#cuerpo")
+cuerpo.innerHTML = ""
+rees.forEach(Ree => {
+cuerpo.innerHTML += `<tr>
+                                <td>${Ree.id}</td>
+                                <td>${Ree.descripcion}</td>
+                                <td>${Ree.importa}</td>
+                                <td><img src="images/cart-24.png" alt="Carrito" title="Agregar al carrito"></td>
+                            </tr>`                          
+ })
+ }
